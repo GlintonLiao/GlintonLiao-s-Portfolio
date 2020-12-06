a15503dcba7247ce01e782adc48fafccd76bbdf1
GlintonLiao <37015336+GlintonLiao@users.noreply.github.com>
2020-12-06 18:50:53 +0800

    update files

--- js/landing.js
+++ js/landing.js
@@ -12,7 +12,6 @@ window.addEventListener("scroll", (e) => {
     let $intro = document.querySelector(".intro");
     let $icon = document.querySelector("#icon");
 
-    let $ui = document.querySelectorAll("#ui");
 
     
     if (scrolled >= 0.01) {
@@ -39,17 +38,29 @@ window.addEventListener("scroll", (e) => {
     };
 
 
-    
-
-
-    if (scrolled >= 0.5) {
-        $ui.style.opacity = 1;
-    };
 
 
 
 
+});
 
+const rows = document.querySelectorAll("#ui ul li");
+const html = document.documentElement;
 
+document.addEventListener("scroll", (e) => {
+  let scrolled = html.scrollTop / (html.scrollHeight - html.clientHeight);
+
+  let total = 1 / rows.length;
+
+  for (let [index, row] of rows.entries()) {
+    let start = total * index;
+    let end = total * (index + 1);
+
+    let progress = (scrolled - start) / (end - start);
+    if (progress >= 1) progress = 1;
+    if (progress <= 0) progress = 0;
+
+    row.style.setProperty("--progress", progress);
+  }
 });
 
